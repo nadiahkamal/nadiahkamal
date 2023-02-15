5291ea7349761fa48e1f0364573d9c67882404e1
nadiahkamal <Nadiah0902@gmail.com>
2023-02-15 08:49:51 +0800

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,6 @@ class App{
         );
         this.cube.position.set(0, 1.5, 4);
         this.scene.add(this.cube);
-        
 
 		this.renderer = new THREE.WebGLRenderer({ antialias: true });
 		this.renderer.setPixelRatio( window.devicePixelRatio );
